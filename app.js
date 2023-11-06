75fe7068e48298574d6edc72c3aa86c7d5dfa498
PeritxCreate <143961072+PeritxCreate@users.noreply.github.com>
2023-11-06 19:49:27 +0000

    orden de loops corregido

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
 
-for (let i = 0; i < pronoun.length; i++) {
-    
-    for (let j = 0; j < adj.length; j++) {
+for (let j = 0; j < adj.length; j++) {
+
+    for (let i = 0; i < pronoun.length; i++) {
         
         for (let k = 0; k < noun.length; k++) {
             
